ebe9773a4dbda2728bf9d9c54f1daaaecf9bb6f6
Piotr Kozubek <piotrkozubek@gmail.com>
2022-02-02 10:37:59 +0100

    Correct package name

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "buffer.js",
     library: {
-      name: "Buffer",
+      name: "buffer",
       type: "umd",
     },
   },
